ba0ccd50631f3344bcb55a68a0c5312e9348901a
Ali Akbar Brohi Web Developer <aliakberbrohi455@gmail.com>
2025-05-04 16:30:33 +0500

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
 // import Services from '@/components/projects'
-import Skills from '@/components/skills'
+// import Skills from '@/components/skills'
 import React from 'react'
 import AboutPage from './About/page'
 import Contact from './Contact/page'
-// import Hero from '@/components/Hero'
+import Hero from '@/components/Hero'
 
 
 function page() {
   return (
     <div>
-      {/* <Hero/> */}
+      <Hero/>
     {/* <Services/> */}
-    <Skills/>
+    {/* <Skills/> */}
     <AboutPage/>
     <Contact/>
     
